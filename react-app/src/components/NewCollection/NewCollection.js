a71a03dbcd28debb541b94595fa4ef589c4aca74
Mirosława Borkowska <130639536+Mirabordem@users.noreply.github.com>
2023-11-19 22:39:16 -0500

    polished css

--- react-app/src/components/NewCollection/NewCollection.js
+++ react-app/src/components/NewCollection/NewCollection.js
@@ -102,10 +102,11 @@ return (
             <label className="single-input">
                 Type
             <input
+                className="types-collection"
                 type="text"
                 value={type}
                 onChange={(e) => setType(e.target.value)}
-                placeholder="(Inside, Out, Mixed)"
+                placeholder="( Inside, Out, Mixed... )"
                 required
             />
             </label>
